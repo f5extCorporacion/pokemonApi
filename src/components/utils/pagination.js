e02d4172a3190016b80eec8ef19d07948ba6a41b
Franklim Valverde <125815174+f5extCorporacion@users.noreply.github.com>
2026-02-24 16:34:32 -0500

    Update pagination.js

--- src/components/utils/pagination.js
+++ src/components/utils/pagination.js
@@ -1,34 +1,24 @@
-const pagination =(items , currentPage)=>{
-    // CANTIDAD DE ITEMS PAGINA
-    const ITEMS_PER_PAGE=20;
+const pagination = (items, currentPage) => {
+  const ITEMS_PER_PAGE = 20;
 
-    //LOS ITEMS DE LA PAGINA ACTUAL
-    const sliceEnd = currentPage * ITEMS_PER_PAGE
-    const sliceStart = sliceEnd - ITEMS_PER_PAGE
-    const itemInCurrentPage = items.slice(sliceStart ,  sliceEnd);
+  const sliceEnd = currentPage * ITEMS_PER_PAGE;
+  const sliceStart = sliceEnd - ITEMS_PER_PAGE;
+  const itemInCurrentPage = items.slice(sliceStart, sliceEnd);
 
-    //ultima pagina
-    const lastpage = Math.ceil(items.length / ITEMS_PER_PAGE)
-  
-    //Bloque actual
-    const PAGES_PER_BLOCK = 5
-    const actualBlock = Math.ceil( currentPage / PAGES_PER_BLOCK)
+  const lastpage = Math.ceil(items.length / ITEMS_PER_PAGE);
 
-    // PAGINAS QUE SE MOSTRARAN EL  BLOQUE ACTUAL
+  const PAGES_PER_BLOCK = 5;
+  const actualBlock = Math.ceil(currentPage / PAGES_PER_BLOCK);
 
-    const pageinCurrentBlock =[]
-    const maxPage = actualBlock * PAGES_PER_BLOCK
-    const minPage = (maxPage - PAGES_PER_BLOCK)+ 1
+  const pageinCurrentBlock = [];
+  const maxPage = actualBlock * PAGES_PER_BLOCK;
+  const minPage = maxPage - PAGES_PER_BLOCK + 1;
 
-    for(let i = minPage; i <= maxPage; i++){
-        if( i <= lastpage){
-            pageinCurrentBlock.push(i)
-        }
-    }
+  for (let i = minPage; i <= Math.min(maxPage, lastpage); i++) {
+    pageinCurrentBlock.push(i);
+  }
 
-  return{ itemInCurrentPage, pageinCurrentBlock, lastpage};
+  return { itemInCurrentPage, pageinCurrentBlock, lastpage };
 };
 
-export{
-    pagination
-}
+export { pagination };
