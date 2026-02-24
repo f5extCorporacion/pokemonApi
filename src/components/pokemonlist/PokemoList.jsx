0f7f6a22d213475412dcdd2a962e35d2da21e8bd
Franklim Valverde <125815174+f5extCorporacion@users.noreply.github.com>
2026-02-24 16:33:26 -0500

    Update PokemoList.jsx

--- src/components/pokemonlist/PokemoList.jsx
+++ src/components/pokemonlist/PokemoList.jsx
@@ -1,12 +1,30 @@
-import CardPoke from "./CardPoke"
-import './../../App.css';
-const PokemoList = ({pokemosProp}) => {
+import CardPoke from "./CardPoke";
+
+const PokemoList = ({ pokemosProp }) => {
   return (
-    <div className="ListaPokemons" >
-        {
-            pokemosProp.map( (poke)=><CardPoke key={poke.url} pokemonLink={poke.url}  />)
-        }
+    <div className="w-full mt-10">
+
+      {/* GRID MODERNO RESPONSIVE */}
+      <div className="
+        grid 
+        gap-10 
+        sm:grid-cols-1
+        md:grid-cols-2
+        lg:grid-cols-3
+        xl:grid-cols-4
+        2xl:grid-cols-5
+        justify-items-center
+      ">
+        {pokemosProp.map((poke) => (
+          <CardPoke
+            key={poke.url}
+            pokemonLink={poke.url}
+          />
+        ))}
+      </div>
+
     </div>
-  )
-}
-export default PokemoList
+  );
+};
+
+export default PokemoList;
