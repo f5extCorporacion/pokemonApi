955d3fa5ec71b27374879ed2d040a7c4d0e7475d
Franklim Valverde <125815174+f5extCorporacion@users.noreply.github.com>
2026-02-24 16:35:45 -0500

    Update App.jsx
    mejorado

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
-
-import Home from'./pages/Home';
+import Home from './pages/Home';
 import Pokemos from './pages/Pokemos';
 import Details from './pages/Details';
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import RoutesPrivate from './components/rutasProtegidas/RoutesPrivate';
-import {Navigate , Outlet} from'react-dom';
 
 function App() {
-
-  //Store
- 
   return (
-    <>
-      <main>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-            {/**Proteccion de Rutas */}
-            <Route element={ <RoutesPrivate/> }>
-                <Route path='/pokemons' element={<Pokemos/>} />
-                <Route path='/details/:pokeid' element={<Details/>} />
-            </Route>
-            {/**Proteccion de Rutas */}
-            {/*<Route path='*' element={<Home/> } />*/}
-        </Routes>
-      </main>
-    </>
-  )
+    <main className="min-h-screen bg-[#e0e5ec]">
+
+      <Routes>
+
+        {/* Home pantalla completa */}
+        <Route path='/' element={<Home />} />
+
+        {/* Layout con container para rutas privadas */}
+        <Route element={<RoutesPrivate />}>
+          <Route
+            element={
+              <div className="max-w-7xl mx-auto px-6 md:px-10 py-8">
+                <Outlet />
+              </div>
+            }
+          >
+            <Route path='/pokemons' element={<Pokemos />} />
+            <Route path='/details/:pokeid' element={<Details />} />
+          </Route>
+        </Route>
+
+      </Routes>
+
+    </main>
+  );
 }
-export default App
+
+export default App;
