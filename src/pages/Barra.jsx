92e68be621038822f099b8aefdef47314555d489
Franklim Valverde <125815174+f5extCorporacion@users.noreply.github.com>
2026-02-24 16:50:45 -0500

    Update Barra.jsx

--- src/pages/Barra.jsx
+++ src/pages/Barra.jsx
@@ -1,15 +1,37 @@
-import menu from'./../assets/menu.png';
-import pokemon from'./../assets/pokemon.png';
-
+import menu from './../assets/menu.png';
+import pokemon from './../assets/pokemon.png';
 
 const Barra = () => {
-
   return (
-    <nav className="abolute">
-        <img src={menu} alt="" />
-        <img src={pokemon} alt=""  className=' pokedex relative -translate-y-[100px] w-[300px]'/>
+    <nav className="sticky top-0 z-50 bg-[#e0e5ec] shadow-[8px_8px_16px_#c8ccd2,-8px_-8px_16px_#ffffff]">
 
-      </nav>
-  )
-}
-export default Barra
+      <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+
+        {/* Botón menú */}
+        <button className="
+          w-12 h-12 rounded-2xl 
+          flex items-center justify-center
+          bg-[#e0e5ec]
+          shadow-[6px_6px_12px_#c8ccd2,-6px_-6px_12px_#ffffff]
+          active:shadow-[inset_4px_4px_8px_#c8ccd2,inset_-4px_-4px_8px_#ffffff]
+          transition-all duration-200
+        ">
+          <img src={menu} alt="menu" className="w-6 h-6" />
+        </button>
+
+        {/* Logo */}
+        <img
+          src={pokemon}
+          alt="pokemon"
+          className="w-40 md:w-52 drop-shadow-md"
+        />
+
+        {/* Espaciador derecho (para balance visual) */}
+        <div className="w-12 h-12" />
+
+      </div>
+    </nav>
+  );
+};
+
+export default Barra;
